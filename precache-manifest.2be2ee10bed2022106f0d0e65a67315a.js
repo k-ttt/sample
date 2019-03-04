42b94d2e4443e940ef4c0af0a8ac85bca859ec83
tetsutaro-kosuga <tetsutaro_kosuga@waku-2.com>
2019-03-04 18:48:57 +0900

    Updates

---
+++ precache-manifest.2be2ee10bed2022106f0d0e65a67315a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "85095b2e3ae4a10d475c",
+    "url": "/sample/static/js/runtime~main.85095b2e.js"
+  },
+  {
+    "revision": "cdbe0068a48b96d3a9f0",
+    "url": "/sample/static/js/main.cdbe0068.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/sample/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "cdbe0068a48b96d3a9f0",
+    "url": "/sample/static/css/main.1df8f757.chunk.css"
+  },
+  {
+    "revision": "913cdaef1fce8546840acf85a9677569",
+    "url": "/sample/index.html"
+  }
+];
